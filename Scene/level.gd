440d4e2fc0216ee3f025189e96a46358b5a031fe
Eric John Adamos <ericjohnadamos@gmail.com>
2024-10-07 23:45:58 +0800

    Set random position for all of the stars under level

--- Scene/level.gd
+++ Scene/level.gd
@@ -4,6 +4,14 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://Scene/meteor.tscn")
 var laser_scene: PackedScene = load("res://Scene/laser.tscn")
 
+func _ready() -> void:
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		var rand_x = rng.randi_range(0, size.x)
+		var rand_y = rng.randi_range(0, size.y)
+		star.position = Vector2(rand_x, rand_y)
+
 func _on_meteor_timer_timeout() -> void:
 	# create an instance
 	var meteor = meteor_scene.instantiate()
